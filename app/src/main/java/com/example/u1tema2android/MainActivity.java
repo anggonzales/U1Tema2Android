e508a12dabdab00b0df25bba26070650061af90e
Gonzales <anggonzalesca@upt.pe>
2020-01-20 11:53:30 -0500

    Agregando DrawerLayout Ejercicio Propuesto

--- app/src/main/java/com/example/u1tema2android/MainActivity.java
+++ app/src/main/java/com/example/u1tema2android/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.u1tema2android;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,12 +15,26 @@ import com.example.u1tema2android.FragmentTabHost.MiFragmentTabHost;
 import com.example.u1tema2android.Multimedia.Multimedia;
 import com.example.u1tema2android.mirecyclerview.MiRecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.drawerlayout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Navigation Drawer
+        DrawerLayout drawer = (DrawerLayout) findViewById(
+                R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(
+                R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override public boolean onCreateOptionsMenu(Menu menu) {
@@ -63,9 +81,58 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-
-
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_compartir:
+                Intent paramView;
+                paramView = new Intent("android.intent.action.SEND");
+                paramView.setType("text/plain");
+                paramView.putExtra("android.intent.extra.TEXT", "Descarga nuestra app de la PlayStore" +
+                        " \n" + "https://play.google.com/store/apps/details?id=app.product.demarktec.alo14_pasajero");
+                startActivity(Intent.createChooser(paramView, "Comparte Nuestro aplicativo"));
+                break;
+            case R.id.accion_salir:
+                finish();
+                break;
+            case R.id.activity_intenciones:
+                startActivity(new Intent(this, Intenciones.class));
+                break;
+
+            case R.id.activity_multimedia:
+                startActivity(new Intent(this, Multimedia.class));
+                break;
+            case R.id.activity_permisos:
+                startActivity(new Intent(this, Permisos.class));
+                break;
+            case R.id.activity_mi_recycler_view:
+                startActivity(new Intent(this, MiRecyclerView.class));
+                break;
+            case R.id.activity_fragment_tab_host:
+            startActivity(new Intent(this, MiFragmentTabHost.class));
+            break;
+            default:
+                break;
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(
+                R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(
+                R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
 }
